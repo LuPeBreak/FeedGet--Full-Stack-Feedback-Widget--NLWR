4ab7c74092a8a4defa3553499c0226472ebf1f92
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2022-05-02 22:01:14 -0300

    Implementado tailwindcss no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+interface ButtonProps {
+  text?: string
+}
 
+function Button(props: ButtonProps){
+  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700">{props.text?? 'Enviar'}</button>
+}
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar"/>
+      <Button text="Ok"/>
+      <Button/>
+    </div>
+  )
 }
 
 export default App
