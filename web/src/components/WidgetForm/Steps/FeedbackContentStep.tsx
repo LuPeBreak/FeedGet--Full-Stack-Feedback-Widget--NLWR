3db75cbfac1d330464897d0ed75fa6b2262214a9
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2022-05-07 08:59:23 -0300

    Web - Implementado envio de dados para cadastro na api

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,72 +1,90 @@
 import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../libs/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
-interface FeedbackContentStepProps{
+interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
-  onFeedbackSend: () => void
+  onFeedbackSend: () => void;
 }
-
 
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
-  onFeedbackSend
-}:FeedbackContentStepProps){
-  const feedbackTypeInfo = feedbackTypes[feedbackType]
+  onFeedbackSend,
+}: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  const [screenshot , setScreenshot] = useState<string|null>(null)
-  const [comment, setComment] = useState('')
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
-  function handleSubmitFeedback(event:FormEvent){
-    event.preventDefault()
-    console.log({
+  async function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+    if(isSendingFeedback){
+      return;
+    }
+    setIsSendingFeedback(true)
+
+    await api.post("/feedbacks", {
+      type: feedbackType,
       comment,
       screenshot,
-    })
-    onFeedbackSend()
+    });
+    setIsSendingFeedback(false)
+    onFeedbackSend();
   }
 
   return (
     <>
-     <header>
-        <button onClick={onFeedbackRestartRequested} type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
-         <ArrowLeft weight="bold" className="w-4 h-4"/>
+      <header>
+        <button
+          onClick={onFeedbackRestartRequested}
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
         <span className="text-xl leading-6 flex items-center gap-2">
-          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
           {feedbackTypeInfo.title}
-          </span>
-        <CloseButton/>
+        </span>
+        <CloseButton />
       </header>
-      
+
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
-        
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que esta acontecendo..."
-          onChange={event=>setComment(event.target.value)}
+          onChange={(event) => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
-
-          <ScreenshotButton 
+          <ScreenshotButton
             screenshot={screenshot}
             onScreenshotTook={setScreenshot}
           />
-          
+
           <button
-            disabled={comment.length ===0}
+            disabled={comment.length === 0}
             type="submit"
             className="disabled:opacity-50 disabled:hover:bg-brand-500 p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
           >
-            Enviar Feedback
+            {
+              isSendingFeedback?<Loading/>:'Enviar Feedback'
+            }
+            
           </button>
         </footer>
       </form>
     </>
-  )
+  );
 }
